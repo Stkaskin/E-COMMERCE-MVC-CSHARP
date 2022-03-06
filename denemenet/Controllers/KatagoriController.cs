6a8cf6b981d667ad99ee6c0101143661817eb5c2
Stkaskin <stkaskin01@gmail.com>
2022-03-06 21:07:52 +0300

    asd
    asd

---
+++ denemenet/Controllers/KatagoriController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace denemenet.Controllers
+{
+    public class KatagoriController : Controller
+    {
+        // GET: Katagori
+        public ActionResult İndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ denemenet/Models/UrunModelView.cs
@@ -0,0 +1,17 @@
+﻿using denemenet.Models.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace denemenet.Models
+{
+    public class UrunModelView
+    {
+        public List<katagori> KatagoriListesi { get; set; }
+        public List<urun> UrunListesi { get; set; }
+        public List<resims> ResimListesi { get; set; }
+       
+        //Sonradan eklencek icin ayrılmış simdilik tek kullanımlık model
+    }
+}
